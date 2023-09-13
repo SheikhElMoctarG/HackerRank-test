badfc632f2e2d84a39967cedaf619a0c46696977
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2023-09-13 08:01:06 +0000

    add plaindromejs form leetcode

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+var isPalindrome = function(x) {
+    return x == intToComplete(x.toString());
+};
+
+function intToComplete(str){
+    var myarr = []
+    for (let i = 0; i < str.length; i++) {
+        myarr.push(str[i])
+    }
+    myarr.reverse()
+    return arrayToInt(myarr);
+}
+
+function arrayToInt(arr){
+    var mystr = '';
+    for (let k = 0; k < arr.length; k++) {
+        mystr += arr[k];
+    };
+    return parseInt(mystr);
+}
